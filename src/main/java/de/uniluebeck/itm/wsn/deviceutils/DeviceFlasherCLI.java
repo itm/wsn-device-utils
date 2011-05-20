e78506c43d2b974e2a834dda9f3df44241761444
Daniel Bimschas <daniel@bimschas.com>
2011-05-20 09:30:47 +0200

    Worked on DeviceFlasher

--- src/main/java/de/uniluebeck/itm/wsn/deviceutils/DeviceFlasherCLI.java
+++ src/main/java/de/uniluebeck/itm/wsn/deviceutils/DeviceFlasherCLI.java
@@ -1,26 +1,19 @@
 package de.uniluebeck.itm.wsn.deviceutils;
 
-import java.io.File;
-
+import com.google.common.io.Files;
+import de.uniluebeck.itm.tr.util.Logging;
+import de.uniluebeck.itm.wsn.drivers.core.async.AsyncCallback;
+import de.uniluebeck.itm.wsn.drivers.core.async.DeviceAsync;
+import de.uniluebeck.itm.wsn.drivers.core.async.OperationQueue;
+import de.uniluebeck.itm.wsn.drivers.core.async.thread.PausableExecutorOperationQueue;
+import de.uniluebeck.itm.wsn.drivers.core.serialport.SerialPortConnection;
+import de.uniluebeck.itm.wsn.drivers.factories.ConnectionFactory;
+import de.uniluebeck.itm.wsn.drivers.factories.DeviceAsyncFactory;
 import org.apache.log4j.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.uniluebeck.itm.tr.util.Logging;
-import de.uniluebeck.itm.wsn.drivers.core.Device;
-import de.uniluebeck.itm.wsn.drivers.core.async.AsyncCallback;
-import de.uniluebeck.itm.wsn.drivers.core.async.DeviceAsync;
-import de.uniluebeck.itm.wsn.drivers.core.async.OperationQueue;
-import de.uniluebeck.itm.wsn.drivers.core.async.QueuedDeviceAsync;
-import de.uniluebeck.itm.wsn.drivers.core.async.thread.PausableExecutorOperationQueue;
-import de.uniluebeck.itm.wsn.drivers.core.operation.ProgramOperation;
-import de.uniluebeck.itm.wsn.drivers.core.serialport.SerialPortConnection;
-import de.uniluebeck.itm.wsn.drivers.jennic.JennicDevice;
-import de.uniluebeck.itm.wsn.drivers.jennic.JennicSerialPortConnection;
-import de.uniluebeck.itm.wsn.drivers.pacemate.PacemateDevice;
-import de.uniluebeck.itm.wsn.drivers.pacemate.PacemateSerialPortConnection;
-import de.uniluebeck.itm.wsn.drivers.telosb.TelosbDevice;
-import de.uniluebeck.itm.wsn.drivers.telosb.TelosbSerialPortConnection;
+import java.io.File;
 
 public class DeviceFlasherCLI {
 
@@ -46,42 +39,22 @@ public class DeviceFlasherCLI {
 			System.exit(1);
 		}
 
-		final SerialPortConnection connection;
-		if ("isense".equals(args[0])) {
-			connection = new JennicSerialPortConnection();
-		} else if ("pacemate".equals(args[0])) {
-			connection = new PacemateSerialPortConnection();
-		} else if ("telosb".equals(args[0])) {
-			connection = new TelosbSerialPortConnection();
-		} else {
-			throw new RuntimeException("Device type \"" + args[0] + "\" unknown!");
-		}
+		final String deviceType = args[0];
+		final String port = args[1];
 
-		connection.connect(args[1]);
+		final SerialPortConnection connection = ConnectionFactory.create(deviceType);
+		connection.connect(port);
 
 		if (!connection.isConnected()) {
 			throw new RuntimeException("Connection to device at port \"" + args[1] + "\" could not be established!");
 		}
 
-		final Device<SerialPortConnection> device;
-		if ("isense".equals(args[0])) {
-			device = new JennicDevice(connection);
-		} else if ("pacemate".equals(args[0])) {
-			device = new PacemateDevice(connection);
-		} else if ("telosb".equals(args[0])) {
-			device = new TelosbDevice(connection);
-		} else {
-			throw new RuntimeException("Device type " + args[0] + " unknown!");
-		}
-
-		ProgramOperation programOperation = device.createProgramOperation();
-		programOperation.setTimeout(120000);
-
-		final OperationQueue queue = new PausableExecutorOperationQueue();
-		DeviceAsync deviceAsync = new QueuedDeviceAsync(queue, device);
+		final OperationQueue operationQueue = new PausableExecutorOperationQueue();
+		final DeviceAsync deviceAsync = DeviceAsyncFactory.create(deviceType, connection, operationQueue);
 
 		AsyncCallback<Void> callback = new AsyncCallback<Void>() {
 			private int lastProgress = -1;
+
 			@Override
 			public void onProgressChange(float fraction) {
 				int newProgress = (int) Math.floor(fraction * 100);
@@ -89,33 +62,37 @@ public class DeviceFlasherCLI {
 					lastProgress = newProgress;
 					log.info("Progress: {}%", newProgress);
 				}
-				
+
 			}
+
 			@Override
 			public void onSuccess(Void result) {
 				log.info("Progress: {}%", 100);
 				log.info("Flashing node done!");
 				connection.shutdown(true);
-				queue.shutdown(true);
+				operationQueue.shutdown(true);
 			}
+
 			@Override
 			public void onFailure(Throwable throwable) {
 				log.error("Flashing node failed with Exception: " + throwable, (Exception) throwable);
 				connection.shutdown(true);
-				queue.shutdown(true);
+				operationQueue.shutdown(true);
 			}
+
 			@Override
 			public void onExecute() {
 				log.info("Starting flash operation...");
 			}
+
 			@Override
 			public void onCancel() {
 				log.info("Flashing was canceled!");
 				connection.shutdown(true);
-				queue.shutdown(true);
+				operationQueue.shutdown(true);
 			}
 		};
-		deviceAsync.program(com.google.common.io.Files.toByteArray(new File(args[2])), 120000, callback);
+		deviceAsync.program(Files.toByteArray(new File(args[2])), 120000, callback);
 
 	}
 
